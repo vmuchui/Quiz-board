a34edd0c180f49e3edea36630b0cb78083785851
vmuchui <kaigongimuchui@gmail.com>
2019-03-29 19:25:03 +0300

    this commit adds id's to the forms.

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,23 @@
-//user logic.
+//back-end logic
+
+$(document).ready(function(){
+  $("#submit").click(function(){
+    var checkResponse = function() {
+      var question1 = document.getElementById('question1').value;
+      var question2 = document.getElementById('question2').value;
+      var question3 = document.getElementById('question3').value;
+      var question4 = document.getElementById('question4').value;
+      var question5 = document.getElementById('question5').value;
+      var question6 = document.getElementById('question6').value;
+    }
+  });
+});
+
+
+
+
+
+//user interface logic starts here:
 $(document).ready(function(){
   $("#button").click(function(){
     $(".card-deck").slideDown();
