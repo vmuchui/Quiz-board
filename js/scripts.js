839eade2e4773cc2f1518c4f95269caa358efd64
vmuchui <kaigongimuchui@gmail.com>
2019-03-30 13:33:06 +0300

    this commit adds the functions to the js file to enable the display.

--- js/scripts.js
+++ js/scripts.js
@@ -2,28 +2,28 @@
 
 $(document).ready(function(){
   $("#submit").click(function(){
-    function checkResponse(question1,question2,question3,question4,question5) {
-      var question1 = document.getElementById('question1').value;
-      var question2 = document.getElementById('question2').value;
-      var question3 = document.getElementById('question3').value;
-      var question4 = document.getElementById('question4').value;
-      var question5 = document.getElementById('question5').value;
-      var question6 = document.getElementById('question6').value;
+    function checkResponse() {
+      var question1 = document.questions.question1.value;
+      var question2 = document.questions.question2.value;
+      var question3 = document.questions.question3.value;
+      var question4 = document.questions.question4.value;
+      var question5 = document.questions.question5.value;
+      var question6 = document.questions.question6.value;
       var answerCorrect = 0;
 
-      if ("#question1 === ECMAjavascript") {
+      if (question1 === "ECMAjavascript") {
         answerCorrect++
       }
-      if ("#question2 === Modulus") {
+      if (question2 === "Modulus") {
         answerCorrect++
       }
-      if ("#question3 === ECMA") {
+      if (question3 === "ECMA") {
         answerCorrect++
       }
-      if ("#question4 === console") {
+      if (question4 === "console") {
         answerCorrect++
       }
-      if ("#question5 === Cascading") {
+      if (question5 === "Cascading") {
         answerCorrect++
       }
     }
