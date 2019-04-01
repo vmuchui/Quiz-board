1104f381eafb54df7dfbc3ea5788ef5b0bc9b659
vmuchui <kaigongimuchui@gmail.com>
2019-04-01 10:23:35 +0300

    This commit adds the Readme file . Also adds the changes made to the scripts.js file changing the backend logic adds the footer to the index.html file. adds a logo to the jumbtron.

--- README.md
+++ README.md
@@ -1,1 +1,39 @@
 # Quiz-board
+## By ** Muchui Victor **
+### Description
+* This is an on line quiz that helps you determine how far you are in terms of the content coverage in your Javascript.It contains questions that are meant to check your ability to retain knowledge.The answers to the questions are in multiple choices from which you should one.
+## Setup installations & Requirements
+* Open the link  to the deployed site to preview it.
+### To access the project locally:
+* Clone the repository from github to your desktop.
+* Open the index.html file and view it on your browser.
+##### You require
+* git
+* chrome browser
+* internet connection.
+## Known Bugs
+* The test is not giving the score as required but i'm working on it.
+## BDD
+In order for you to use the program appropriately you need to enter an answer from the given choices.
+* This is the Behaviour of the app:
+Behaviour                         |Output
+----------------------------------|--------------------------------
+No answers given                  |Please retake your test!!
+All questions answered            |You have answered all questions
+Five correct answers              |You are awesooome!
+More than two correct answers     |Fairly done
+Less than two correct answers     |Please consider redoing the test
+# Technologies Used
+* HTML.
+* Bootstrap 4 framework
+* CSS styling Sheets
+* Javascript
+* jquery library
+* Atom.
+## Support and Contact Details
+* call +254 717 304 712
+* twitter @muchuivictor
+* email me at kaigongimuchui@gmail.com
+# License & Copyright Details
+* MIT license
+* (C)2019 Moringa School.
--- js/scripts.js
+++ js/scripts.js
@@ -1,54 +1,85 @@
 //back-end logic
-function checkResponse(){
-  var question1 = document.getElementById('question1').value;
-  var question2 = document.getElementById('question2').value;
-  var question3 = document.getElementById('question3').value;
-  var question4 = document.getElementById('question4').value;
-  var question5 = document.getElementById('question5').value;
-  var answerCorrect = 0;
+$(document).ready(function() {
+  $("#submit").click(function() {
+    var arrayFirst = [];
+    for (i = 1; i <= 5; i++) {
+      query = "quiz" + i;
+      arrayFirst.push(query);
+    }
+    var inputs = document.getElementById("questions").elements;
+    var score = 0;
+    for (var i = 0; i < inputs.length; i++) {
+      if (inputs[i].type == 'radio' && inputs[i].checked) {
+        score++;
+      }
+    }
+    if (score < 5) {
+      alert("Please retake your test!!");
+      document.location.reload();
+    } else {
+      alert("You have answered all the questions!!!Good Job.");
+    }
 
-  if (question1 == "ECMAjavascript".value) {
-    answerCorrect++;
-  }
-  if (question2 == "Modulus".value) {
-    answerCorrect++;
-  }
-  if (question3 == "ECMA".value) {
-    answerCorrect++;
-  }
-  if (question4 == "Javascript console".value) {
-    answerCorrect++;
-  }
-  if (question5 == "Cascading Style Sheets".value) {
-    answerCorrect++;
-  }
-  alert(answerCorrect)
-}
 
+    var arrSecond = [];
+    for (var index = 0; index < arrayFirst.length; index += 1) {
+      var questions = parseInt($("input[name=" + arrayFirst[index] + "]:checked").val());
+      arrSecond.push(questions);
+    }
+    var total = 0;
+    for (var index = 0; index < arrSecond.length; index += 1) {
+      total += arrSecond[index];
+    }
+    document.getElementById('finalscore').innerHTML = "you have scored " + total + "%";
+    if (total >= 80) {
+      document.getElementById('message').innerHTML = "You are awesooome!";
+    } else if (total >= 50 && total < 80) {
+      document.getElementById('message').innerHTML = "Fairly done";
+    } else {
+      document.getElementById('message').innerHTML = "Please consider redoing the test";
+    }
+
+
+  });
+});
+
+// scrolldown bar
+$(document).ready(function() {
+  $(window).scroll(function() {
+    var a = $(window).scrollTop(); //shows position of the scroll in pixels
+    var b = $(document).height(); //full height of doc
+    var c = $(window).height(); //window height
+    scrollPercent = (a / (b - c)) * 100;
+    var position = scrollPercent;
+
+    $("#progressbar").attr('value', position); //setting the value of the progressbar
+
+  });
+});
 
 
 
 
 //user interface logic starts here:
-$(document).ready(function(){
-  $("#button").click(function(){
+$(document).ready(function() {
+  $("#button").click(function() {
     $("#showFirst").slideDown();
     $("#openNext").slideDown();
     $(".disappear").fadeOut();
   });
-  $("#openNext").click(function(){
+  $("#openNext").click(function() {
     $("#showSecond").fadeIn();
     $("#openLast").fadeIn();
     $("#showFirst").fadeOut();
     $("#openNext").fadeOut();
   });
-  $("#openLast").click(function(){
+  $("#openLast").click(function() {
     $("#showLast").fadeIn();
     $("#submit").fadeIn();
+    $("#reload").fadeIn();
     $("#showSecond").fadeOut();
     $("#openLast").fadeOut();
+    $("#footer").slideDown();
   });
-  $("#submit").click(function(){
-    $("#afterSubmit").show();
-  })
+
 });
